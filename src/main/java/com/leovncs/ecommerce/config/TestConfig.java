1864b0f2809d1b35350a865214c0002056a7de5a
Leonardo Vinicius <leoovncs@gmail.com>
2024-07-04 16:11:25 -0300

    OrderStatus enum

--- src/main/java/com/leovncs/ecommerce/config/TestConfig.java
+++ src/main/java/com/leovncs/ecommerce/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.leovncs.ecommerce.config;
 
 import com.leovncs.ecommerce.entities.Order;
 import com.leovncs.ecommerce.entities.User;
+import com.leovncs.ecommerce.entities.enums.OrderStatus;
 import com.leovncs.ecommerce.repositories.OrderRepository;
 import com.leovncs.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Kauane Fernanda", "kauane@gmail.com", "988888888", "12345678");
         User u2 = new User(null, "Victória Lais", "victoria@gmail.com", "977777777", "12345678");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
