801be8da2f65796e7cb6ebf92b54174b9f3731b8
Leonardo Vinicius <leoovncs@gmail.com>
2024-07-08 13:51:56 -0300

    Product

---
+++ src/main/java/com/leovncs/ecommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.leovncs.ecommerce.services;
+
+import com.leovncs.ecommerce.entities.Product;
+import com.leovncs.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
